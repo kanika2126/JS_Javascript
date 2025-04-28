91b53841be8407f82869984a96901a10b102809a
kanika2126 <kanikadaksh456@gmail.com>
2025-04-28 06:18:02 +0000

    conversion datatype

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,25 @@
+let score = false
+
+//console.log(typeof score);
+
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log( valueInNumber);
+
+//"33" => 33
+//33ab' =>NAN
+// true = 1
+
+let isLoggedIn = "kanika"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1 = true
+//"" = false
+//"kanika" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
