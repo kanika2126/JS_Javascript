42cb6f3bd4183af63fe25d78f1c27d5042922d59
kanika2126 <kanikadaksh456@gmail.com>
2025-04-28 05:36:31 +0000

    A code for js

---
+++ 01_Basics/01_var.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "kanika@google.com"
+var accountPassword = "12344"
+accountCity = "Jaipur"
+
+accountEmail ="abc@google.com"
+accountPassword = "21212121"
+accountCity = "delhi"
+
+console.log(accountId);
+console.log(accountEmail);
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
