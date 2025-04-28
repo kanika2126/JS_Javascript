d9db7b7b7fa4e0e8263fd7699aa111680a0b7b72
kanika2126 <kanikadaksh456@gmail.com>
2025-04-28 07:05:55 +0000

    conversion

--- 01_Basics/conversion.js
+++ 01_Basics/conversion.js
@@ -1,4 +1,4 @@
-let score = false
+/*let score = false
 
 //console.log(typeof score);
 
@@ -22,4 +22,27 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+console.log(stringNumber);*/
+
+//***************************Operations********************** */
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "Hello"
+let str2= "Kanika"
+let str3 = str1 + str2
+//console.log(str3);
+//console.log("1"+2);
+//console.log("2" + 1);
+
+console.log(+true);
+console.log(+"");
+
