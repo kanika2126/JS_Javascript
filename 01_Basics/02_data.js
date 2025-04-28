908531ef3c330e79a86f6edc70d26ad74f78bc7b
kanika2126 <kanikadaksh456@gmail.com>
2025-04-28 05:58:57 +0000

    a code for js

---
+++ 01_Basics/02_data.js
@@ -0,0 +1,24 @@
+"user Strict ";// treat all js code as newer version
+
+//alert( 3+3)// we are using node js not browser
+
+console.log(3+3)
+console.log("Kanika")
+
+
+let name = "kanika"
+let agr = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to pwr 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standlone value
+//undefind =>
+//symbol => unique
+
+//object
+
+console.log(typeof age);
